4debef80aaa5e155d1f07c0a607fd5a34ac70bff
Pearwa-likeLion <fittokung@gmail.com>
2024-07-02 20:07:34 +0700

    first commit

---
+++ ProjectStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
